15f4cfda2ef9419f99571cacfe972a82a676aba9
obikiel <ezekielobiomachi1@gmail.com>
2024-07-13 16:43:14 +0100

    update

--- main.tf
+++ main.tf
@@ -9,15 +9,14 @@ resource "azurerm_resource_group" "resource_group" {
 
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "obikiel"
-  id                  = "first"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
-  sku_name            = "Basic"
+  sku_name            = "B1"
   os_type             = "Windows"
 }
 
 resource "azurerm_windows_web_app" "windows_web_app" {
-  name                = "obikiel_app"
+  name                = "izzy-windows-app"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   service_plan_id     = azurerm_service_plan.app_service_plan.id 
